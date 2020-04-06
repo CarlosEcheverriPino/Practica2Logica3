f4a660ad32212262a70606416714d0ef64217658
CompucenterPC1 <CompucenterPC1@Carlos>
2020-04-05 19:06:07 -0500

    Ya se logro leer el archivo, toca darle estructura al archivo y analizar como vamos a crear a cada de las personas para enviarlas a la lista si el archivo contiene informacion de X personas

--- src/practica2/Archivo.java
+++ src/practica2/Archivo.java
@@ -5,7 +5,7 @@
  */
 package practica2;
 
-import java.io.BufferedReader;
+
 import java.io.*;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
@@ -19,32 +19,45 @@ public class Archivo {
     // estos arreglos almacenara los valores de las matrices y de las tripletas
     int valores[];
     
-    public Archivo() {
-    }
-    
- // metodo por el cual se lee el archivo y se convierte en una cadena de caracteres
-    public String leerTxt(String direccion){
-        String texto = "";//---> variable que almacenara los datos en una linea de texto 
-        
-        try {
-            BufferedReader bf = new BufferedReader(new FileReader(direccion));//--->recorre el ciclo mientras haya datos 
-            String temp= "";
-            String bfread;
-            while ((bfread = bf.readLine()) != null){
-                temp = temp + bfread+" ";
-                
-            }
-            texto = temp;
-            
-        } catch (IOException e) {
-            System.out.println("No se encontro archivo");
+public String leerTxt(String direccion){
+    String texto  = "";
+    try {
+        BufferedReader bf = new BufferedReader(new FileReader(direccion));//--> Se inicializa la clase BufferReader y se 
+        String temp = "";                                                 //le enviar ruta destino del archivo.
+        String bfRead;
+        while((bfRead = bf.readLine()) != null){                          //-->Recorre el ciclo mientras tenga datos
+            temp = temp + bfRead;                                         //-->Concatenamos los datos.  
         }
-        return texto;
+        texto = temp;                                                     //-->Cambiamos a la variable principal.  
+        
+    } catch (Exception e) {
+        System.out.println("No se encontro archivo");
     }
+    return texto;
+}
+
+public StringBuilder dividirTxt(FileReader a){
+    StringBuilder cadena = new StringBuilder();
+    FileReader fr =a;
+    String s;
+    try {
+        int valor = fr.read();
+        String letra = "";
+        valor = fr.read();
+        while (valor != -1){
+            char texto = (char)valor;
+            cadena.append(texto);
+            valor = fr.read();
+        }
+    } catch (Exception e) {
+    }
+    return cadena;
+  
+   
 } 
  
     
-       
+}       
                     
                     
                
